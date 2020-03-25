b634b0616f3416d7b7bef63ba36d167469ed0154
Alex Mendes <alexmendes.unix@gmail.com>
2020-03-25 11:03:28 -0300

    Convert from python 2 to python 3 (print syntax).

--- codeacademy/battleship.py
+++ codeacademy/battleship.py
@@ -10,7 +10,7 @@ for x in range(5):
 # Formatando o Tabuleiro (eliminando caracteres).
 def print_board(board):
   for row in board:
-    print " ".join(row)
+    print(" ".join(row))
 
 # Printando o Tabuleiro Formatado para jogadas.
 print_board(board)
@@ -35,29 +35,29 @@ ship_col = random_col(board)
 for turn in range(4):
 
 # Validacao de Entrada de Dados.
-  guess_row = int(raw_input("Guess Row: "))
-  guess_col = int(raw_input("Guess Col: "))
+  guess_row = int(input("Guess Row: "))
+  guess_col = int(input("Guess Col: "))
 
   if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sunk my battleship!"
+    print("Congratulations! You sunk my battleship!")
     break
 
   else:
     if turn == 3:
       board[guess_row][guess_col] = "X"
-      print "Game Over."
+      print("Game Over.")
 
     else:
       if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-        print "Oops, that's not even in the ocean."
+        print("Oops, that's not even in the ocean.")
         reason = "Out of Map!"
       elif(board[guess_row][guess_col] == "X"):
-        print "You guessed that one already."
+        print("You guessed that one already.")
         reason = "Repeated Guesses!"
       else:
-        print "You missed my battleship!"
+        print("You missed my battleship!")
         board[guess_row][guess_col] = "X"
   
 # Print (turn + 1) here!
-  print "Turn", turn + 1
+  print("Turn", turn + 1)
   print_board(board)
