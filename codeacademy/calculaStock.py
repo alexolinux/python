c8f4db3927cbcd4ad5ec24f72c5e8a5fe780c3b6
Alex Mendes <alexmendes.unix@gmail.com>
2020-04-01 15:22:29 -0300

    Edited two existents files from codeacademy.

--- codeacademy/calculaStock.py
+++ codeacademy/calculaStock.py
@@ -1,7 +1,6 @@
 # About Dictioraries:
 # dict = {"key" : value}
 
-
 # Estudo para percorrer listas e calcular quantidade e valor de produtos.
 # O desafio fica para calcular o valor total de produtos em estoque.
 
@@ -16,25 +15,25 @@ prices = {
   "banana" : 4,
   "apple" : 2,
   "orange" : 1.5,
-  "pear"	:	3  
+  "pear" : 3
 }
 print ('Dict prices has'), len(prices), 'price items.\n'
 
 # Deve-se criar esta variável zerada para ser utilizada
 # futuramente para calcular o valor total dos produtos
-# em estoque. O esquema é criar fora do loop...
+# em estoque. O esquema é "criá-la fora do loop"...
 
 total = 0
 
 for product in prices:
-  print product
-  print "Price: %.2f" % prices[product]
-  print "Stock: %d" % stock[product]
+  print(product)
+  print("Price: %.2f" % prices[product])
+  print("Stock: %d" % stock[product])
   total_by_product = prices[product] * stock[product]
   total += total_by_product
   
-  print
-  print 'Valor Total de', product, '= R$', total_by_product
-  print
+  print()
+  print('Valor Total de', product, '= R$', total_by_product)
+  print()
 
-print "Valor Total: ", total
+print("Valor Total: ", total)
