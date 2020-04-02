683f85f3e2f1091a7c5df266f3836fb62bd1ea02
Alex Mendes <alexmendes.unix@gmail.com>
2020-04-02 09:50:40 -0300

    Add getKeyDict.py lesson in codeacademy repo.

---
+++ codeacademy/getKeyDict.py
@@ -0,0 +1,11 @@
+"""Catch title of movie from dictionary"""
+
+buster = {
+    "Romance" : "Nothing Hill",
+    "Horror" : "Evil Dead II",
+    "Comedy" : "The First Wives Club",
+    "Supense": "A Dangerous Place"
+}
+
+for title in buster:
+    print(buster[title])
