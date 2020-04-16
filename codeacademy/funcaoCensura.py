40e9ab52e7062358e1e1fcb00c9287e6485ddc16
Alex Mendes <alexmendes.unix@gmail.com>
2020-04-16 10:52:00 -0300

    Add comments

--- codeacademy/funcaoCensura.py
+++ codeacademy/funcaoCensura.py
@@ -1,8 +1,14 @@
+"""
+This function catches a sentence and filters word to changing letters for stars
+"""
+
 def censor(text, word):
+    #Separate words in a phrase
     words = text.split()
     result = ''
     stars = '*' * len(word)
     count = 0
+
     for i in words:
         if i == word:
             words[count] = stars
