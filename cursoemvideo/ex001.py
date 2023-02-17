d0d89df39b01acea93524a4dc362245771f83ea9
alexmbarbosa <alexmendes.unix@gmail.com>
2023-02-17 23:43:45 +0000

    Start Curso em Video exercises.

---
+++ cursoemvideo/ex001.py
@@ -0,0 +1,4 @@
+# 001: Simple "Hello World" on screen.
+
+msg = "Hello World" 
+print(msg)
---
+++ cursoemvideo/ex002.py
@@ -0,0 +1,6 @@
+# 002: Replying a user.
+
+name = input("Enter with your name: \n")
+
+print("Current User: \n{}".format(name))
+
